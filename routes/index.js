6881a5a04376fbed3672c26402fc9d7e042bc381
Ivan Loh <ivan@eriad.com>
2014-12-17 02:04:42 +0800

    clean up app

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router  = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'mygamedev' });
+  res.render('index', { title: 'mygamedev', page: 'index' });
 });
 
 module.exports = router;
