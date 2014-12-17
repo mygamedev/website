a0b3667cab40d3b4a5ec4932ef30bdd58fb41591
Ivan Loh <ivan@eriad.com>
2014-12-18 00:14:45 +0800

    base skeleton for the rest of the pages

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@
 var express = require('express'),
     router  = express.Router();
 
-router.get('/',          function (req, res) { res.render('index',     {title: 'mygamedev',           page: 'index'}); });
-router.get('/about',     function (req, res) { res.render('about',     {title: 'about mygamedev',     page: 'about'}); });
-router.get('/news',      function (req, res) { res.render('news',      {title: 'mygamedev news',      page: 'news'}); });
-router.get('/directory', function (req, res) { res.render('directory', {title: 'mygamedev directory', page: 'directory'}); });
-router.get('/faq',       function (req, res) { res.render('faq',       {title: 'mygamedev faq',       page: 'faq'}); });
+router.get('/',          function (req, res) { res.render('page_index',     {title: 'mygamedev',           page: 'index'});     });
+router.get('/about',     function (req, res) { res.render('page_about',     {title: 'about mygamedev',     page: 'about'});     });
+router.get('/news',      function (req, res) { res.render('page_news',      {title: 'mygamedev news',      page: 'news'});      });
+router.get('/directory', function (req, res) { res.render('page_directory', {title: 'mygamedev directory', page: 'directory'}); });
+router.get('/faq',       function (req, res) { res.render('page_faq',       {title: 'mygamedev faq',       page: 'faq'});       });
+router.get('*',          function (req, res) { res.redirect('/');                                                               });
 
 module.exports = router;
