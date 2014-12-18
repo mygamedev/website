5477e11e048dd4426d52367fed480f7b104210c1
Ivan Loh <ivan@eriad.com>
2014-12-19 00:52:50 +0800

    the pictures

---
+++ public/js/directory.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  function hideall() {
+    $('#gamedev').hide();
+    $('#multibiz').hide();
+    $('#artoutsource').hide();
+    $('#gamepublishers').hide();
+    $('#institutions').hide();
+    $('#incubator').hide();
+  }
+
+  hideall();
+
+  $('#gamedevb').on('click',        function () { hideall(); $('#gamedev').show(); });
+  $('#multibizb').on('click',       function () { hideall(); $('#multibiz').show(); });
+  $('#artoutsourceb').on('click',   function () { hideall(); $('#artoutsource').show(); });
+  $('#gamepublishersb').on('click', function () { hideall(); $('#gamepublishers').show(); });
+  $('#institutionsb').on('click',   function () { hideall(); $('#institutions').show(); });
+  $('#incubatorb').on('click',      function () { hideall(); $('#incubator').show(); });
+
+}());
