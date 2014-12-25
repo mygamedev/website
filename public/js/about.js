c1b3f2665a2a09100d9c5f16d0755dd406a5adb8
Ivan Loh <ivan@eriad.com>
2014-12-25 22:49:42 +0800

    cleanup a bit

--- public/js/about.js
+++ public/js/about.js
@@ -1,12 +1,12 @@
 (function ($) {
+  'use strict';
 
   $('.partner').each(function () {
 
     var $this = $(this);
-
-    $this.css('opacity', '0.2')
-         .on('mouseenter', function () { $this.css('opacity', '0.9'); })
-         .on('mouseleave', function () { $this.css('opacity', '0.2'); })
+        $this.css('opacity', '0.2')
+             .on('mouseenter', function () { $this.css('opacity', '0.9'); })
+             .on('mouseleave', function () { $this.css('opacity', '0.2'); })
 
   });
 
