21068f7851bf46900f7bfd9eca484b773431eab5
Ivan Loh <ivan@eriad.com>
2015-01-03 23:05:58 +0800

    responsive video page

---
+++ public/js/news.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+  $(document).ready(function () {
+
+
+    // "responsive" stage
+    var $stage       = $('#stage'),
+        $video       = $('#videoframe'),
+        stage_height = $stage.height(),
+        video_height = $video.height();
+
+    $video.height($video.width() / 96 * 54);
+    $stage.height(stage_height - (video_height - $video.height()));
+
+    $(window).resize(function () {
+      $video.height($video.width() / 96 * 54);
+      $stage.height(stage_height - (video_height - $video.height()));
+    });
+
+
+
+  });
+
+}($));
